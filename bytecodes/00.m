cf2890e10760f80426fc31a9eae03424c35472d2
Ravan Asad <ravanasad@outlook.com>
2024-01-02 08:59:31 -0800

    Updated helpers.c & main.c

--- bytecodes/00.m
+++
@@ -1,7 +0,0 @@
-push 1
-push 2
-push 223
-push 6
-push 356
-push 3
-pall
--- helpers.c
+++ helpers.c
@@ -1,19 +1,33 @@
-#include "main.h"   
+#include "main.h"
 
-instruction_t opcodes[] = {
-    {"push", f_push},
-    {"pall", f_pall},
-    {NULL, NULL}
-};
-
+/**
+ * find_func - find the correct function
+ * @opcode: function code
+ * Return: function and function opcode
+ */
 instruction_t *find_func(char *opcode)
 {
-    int i = 0;
-    while (opcodes[i].opcode != NULL)
-    {
-        if (strcmp(opcode, opcodes[i].opcode) == 0)
-            return (&opcodes[i]);
-        i++;
-    }
-    return (NULL);
+	int i = 0;
+	instruction_t opcodes[] = {
+		{"push", f_push},
+		{"pall", f_pall},
+		{NULL, NULL}
+	};
+	instruction_t *result = NULL;
+
+	while (opcodes[i].opcode != NULL)
+	{
+		if (strcmp(opcode, opcodes[i].opcode) == 0)
+		{
+			result = malloc(sizeof(instruction_t));
+			if (result == NULL)
+			{
+				printf("Error: malloc failed\n");
+				exit(EXIT_FAILURE);
+			}
+			return (result);
+		}
+		i++;
+	}
+	return (NULL);
 }
