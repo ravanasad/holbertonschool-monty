d24946adb89040be416f5814038df43d9014073e
tync0 <isgenderlituncay2004@gmail.com>
2024-01-11 05:16:08 -0800

    done

--- stack_funcs.c
+++ stack_funcs.c
@@ -95,22 +95,3 @@ void f_swap(stack_t **stack, unsigned int line_number)
 	tmp->n = tmp->next->n;
 	tmp->next->n = tmp_n;
 }
-/**
- * f_add - adds the top two elements of the stack
- * @stack: pointer to the stack
- * @line_number: the line number in the file
- * Return: void
- */
-void f_add(stack_t **stack, unsigned int line_number)
-{
-	stack_t *tmp = *stack;
-
-	if (*stack == NULL || (*stack)->next == NULL)
-	{
-		fprintf(stderr, "L%u: can't add, stack too short\n", line_number);
-		exit(EXIT_FAILURE);
-	}
-	tmp->next->n = tmp->next->n + tmp->n;
-	*stack = tmp->next;
-	free(tmp);
-}
