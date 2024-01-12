fa4b7cc3368a5fd182ddaaf7f756b8bc817bab1f
Ravan Asad <ravanasad@outlook.com>
2024-01-12 11:43:08 -0800

    FPRINTF error finally founded

--- main.c
+++ main.c
@@ -20,7 +20,7 @@ int main(int argc, char *argv[])
 	fd = fopen(argv[1], "r");
 	if (fd == NULL)
 	{
-		printf("Error: Can't open file %s\n", argv[1]);
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
 		exit(EXIT_FAILURE);
 	}
 	while (getline(&line, &len, fd) != -1)
